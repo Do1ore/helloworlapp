f3314f3f3998f49afee1e790277a1bfa2b6b5138
Vladislav Kharlamov <vladislav.kharlamov@krainet.by>
2025-01-04 17:03:50 +0300

    super feature

--- Program.java
+++ Program.java
@@ -1,7 +1,10 @@
 public class Program {
 
     private static final String MESSAGE = "LB 3";
+    private static final String MESSAGE_FROM_DEVELOP_BRANCH = "LB 3";
+
     public static void main(String[] args) {
         System.out.println("Hello " + MESSAGE);
+        System.out.println("New message:  " + MESSAGE_FROM_DEVELOP_BRANCH);
     }
 }
