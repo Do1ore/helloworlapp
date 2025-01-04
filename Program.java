1732416b8c1b0464b0ba89f9e871e697548e2cb8
Vladislav Kharlamov <vladislav.kharlamov@krainet.by>
2025-01-04 17:01:15 +0300

    hello world message changed

--- Program.java
+++ Program.java
@@ -1,5 +1,7 @@
 public class Program {
+
+    private static final String MESSAGE = "LB 3";
     public static void main(String[] args) {
-        System.out.println("Hello world 1");
+        System.out.println("Hello " + MESSAGE);
     }
 }
